459e8946cb69b362355881d0a7a7d4dbec4cab25
pirozas <138485153+pirozas@users.noreply.github.com>
2024-04-20 16:29:34 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pip install netaddr requests ipaddress
+
+wget https://raw.githubusercontent.com/pirozas/pirozas/main/hdig.py
+
+
+https://raw.githubusercontent.com/pirozas/pirozas/main/hostdigger
+
+
+mv hostdigger $PREFIX/bin/hostdigger
+mv hdig.py $PREFIX/bin/hdig.py
+
+
+
+ 
